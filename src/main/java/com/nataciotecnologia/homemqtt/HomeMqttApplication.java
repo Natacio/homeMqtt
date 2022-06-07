65d70272a8ed5a0ed64f2c66acc77279d82f6dc6
Natacio <natacio07@gmail.com>
2022-06-07 07:50:47 -0300

    first commit

---
+++ src/main/java/com/nataciotecnologia/homemqtt/HomeMqttApplication.java
@@ -0,0 +1,13 @@
+package com.nataciotecnologia.homemqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeMqttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeMqttApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nataciotecnologia/homemqtt/HomeMqttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nataciotecnologia.homemqtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeMqttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
